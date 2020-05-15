643e560d8c9baeb707cddd0468380198bf52509e
Diego Lopes <diego-chez@hotmail.com>
2020-05-15 12:51:58 -0300

    Enable cors

--- server/config/server.js
+++ server/config/server.js
@@ -1,9 +1,12 @@
 const port = 3003;
 
 const express = require("express");
+const cors = require("../config/cors");
 const server = express();
 
 server.use(express.urlencoded({ extended: true }));
+server.use(express.json());
+server.use(cors);
 
 server.listen(port, () => {
   console.log("[ SERVER ]", "Running on port", port);
