7ce78fdc92cc33f0293abf0155e7166625d1da84
Diego Lopes <diego-chez@hotmail.com>
2020-05-16 14:58:17 -0300

    Create form and list components

--- client/src/views/Todo/index.jsx
+++ client/src/views/Todo/index.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import PageHeader from '@/components/PageHeader'
+import TodoForm from '@/components/Todo/Form'
+import TodoList from '@/components/Todo/List'
+
 
 export default props => {
-
     return (
         <div>
             <PageHeader name="Tarefas" small="Cadastro"/>
+            <TodoForm />
+            <TodoList />
         </div>
     )
 }
