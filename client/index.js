fcb3b0f8b81468ae4be0a3f7f6557723160c8105
Diego Lopes <diego-chez@hotmail.com>
2020-05-16 09:23:10 -0300

    Setup webpack to work with react

---
+++ client/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const el = document.getElementById("root");
+ReactDOM.render(
+  <div>
+    <h1>Hello</h1>
+    <Btn>Click</Btn>
+  </div>,
+  el
+);
---
+++ webpack.config.js
@@ -0,0 +1,69 @@
+const isDevMode = process.env.NODE_ENV !== "production";
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+
+module.exports = {
+  mode: isDevMode ? "development" : "production",
+  entry: "./client/index.js",
+  output: {
+    path: path.resolve(__dirname, "client", "build"),
+    filename: "bundle.js",
+  },
+  optimization: {
+    minimizer: [
+      new OptimizeCssAssetsPlugin({}),
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+        uglifyOptions: {
+          output: {
+            comments: false,
+          },
+        },
+      }),
+    ],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "styles.css",
+    }),
+    new HtmlWebPackPlugin({
+      template: path.resolve(__dirname, "client", "public", "index.html"),
+      filename: "index.html",
+      favicon: path.resolve(__dirname, "client", "public", "favicon.ico"),
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.s?[ac]ss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+          },
+        ],
+      },
+      {
+        test: /\.(ico|png|jpg|jpeg|gif|svg)$/,
+        use: ["file-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+};
