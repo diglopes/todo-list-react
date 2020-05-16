62210086f741440a5c72cc606adc915c2f581f9a
Diego Lopes <diego-chez@hotmail.com>
2020-05-16 12:10:37 -0300

    Create about and todo views

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import GlobalStyle from './styles'
+import Todo from './views/Todo'
+import About from './views/About'
+
 import '@modules/bootstrap/dist/css/bootstrap.min.css'
 import '@modules/font-awesome/css/font-awesome.min.css'
 
 export default () => (
     <>
-        <h1 className="display-1">Hello</h1>
-        <button className="btn btn-info">Click</button>
+        <Todo />
+        <About />
         <GlobalStyle/>
     </>
 )
---
+++ client/src/views/Todo/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => {
+
+    return (
+        <div>
+            <h1>Todo</h1>
+        </div>
+    )
+}
