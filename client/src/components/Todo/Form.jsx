fac59b26a25ec9d5ab93678c3f8da1d21ca7fcf0
Diego Lopes <diego-chez@hotmail.com>
2020-05-16 15:26:44 -0300

    Add form component content

--- client/src/components/Todo/Form.jsx
+++ client/src/components/Todo/Form.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
 
 export default props => (
-    <div>
-        <h2>Form</h2>
+    <div role="form">
+        <div className="row">
+            <div className="col-sm-12 col-md-9 col-lg-10">
+                <input 
+                    type="text" 
+                    id="description" 
+                    className="form-control" 
+                    placeholder="Adicione uma tarefa"
+                    />
+            </div>
+
+            <div className="col-sm-12 col-md-3 col-lg-2">
+                <button className="btn btn-primary">
+                    <i className="fa fa-plus"></i>
+                </button>
+            </div>
+        </div>
     </div>
 )
