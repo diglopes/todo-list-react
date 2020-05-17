ddbb20fe0ddd8b55813ffbfb1cf0e9efab380c8b
Diego Lopes <diego-chez@hotmail.com>
2020-05-17 07:33:58 -0300

    Fix routes bug

--- server/config/routes.js
+++ server/config/routes.js
@@ -2,15 +2,14 @@ const express = require("express");
 const path = require("path");
 
 module.exports = function (server) {
-  server.get("/*", (req, res) => {
+  const router = express.Router();
+  server.use("/api", router);
+  const todoService = require("../api/todo/todo-service");
+  todoService.register(router, "/todo");
+
+  server.get("*", (req, res) => {
     res.sendFile(
       path.join(__dirname, "..", "..", "client", "build", "index.html")
     );
   });
-
-  const router = express.Router();
-  server.use("/api", router);
-
-  const todoService = require("../api/todo/todo-service");
-  todoService.register(router, "/todo");
 };
