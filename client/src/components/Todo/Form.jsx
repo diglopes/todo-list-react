f8974a1d45c8a4f632e73ea75a4208a2fa9adceb
Diego Lopes <diego-chez@hotmail.com>
2020-05-17 10:22:02 -0300

    Add hotkeys

--- client/src/components/Todo/Form.jsx
+++ client/src/components/Todo/Form.jsx
@@ -2,25 +2,36 @@ import React from 'react'
 import GridColumn from '@/components/GridColumn'
 import IconButton from '@/components/IconButton'
 
-export default props => (
-    <div role="form">
-        <div className="row">
-            <GridColumn cols="12 9 10">
-                <input 
-                    type="text" 
-                    id="description" 
-                    className="form-control" 
-                    placeholder="Adicione uma tarefa"
-                    value={props.description}
-                    onChange={e => props.handleDescriptionChange(e)}
-                />
-            </GridColumn>
+export default props => {
+    const keyHandler = (e) => {
+        if(e.key === 'Enter') {
+            e.shiftKey ? props.handleSearch() : props.handleAdd()
+        } else if (e.key === 'Escape') {
+            props.handleClear()
+        }
+    }
 
-            <GridColumn cols="12 3 2" className="col-sm-12 col-md-3 col-lg-2">
-                <IconButton style="primary" icon="plus" onClick={props.handleAdd}/>
-                <IconButton style="info" icon="search" onClick={props.handleSearch}/>
-                <IconButton style="light" icon="close" onClick={props.handleClear}/>
-            </GridColumn>
+    return (
+        <div role="form">
+            <div className="row">
+                <GridColumn cols="12 9 10">
+                    <input 
+                        type="text" 
+                        id="description" 
+                        className="form-control" 
+                        placeholder="Adicione uma tarefa"
+                        value={props.description}
+                        onChange={e => props.handleDescriptionChange(e)}
+                        onKeyUp={keyHandler}
+                    />
+                </GridColumn>
+    
+                <GridColumn cols="12 3 2" className="col-sm-12 col-md-3 col-lg-2">
+                    <IconButton style="primary" icon="plus" onClick={props.handleAdd}/>
+                    <IconButton style="info" icon="search" onClick={props.handleSearch}/>
+                    <IconButton style="light" icon="close" onClick={props.handleClear}/>
+                </GridColumn>
+            </div>
         </div>
-    </div>
-)
+    )
+}
